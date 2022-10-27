d2735e21f784204dda562aa411995b4cf445d117
Wei Xi Huang <weixih@nycstudents.net>
2022-10-26 21:42:44 -0400

    eeeeeeeeee

--- src/Runner.java
+++ src/Runner.java
@@ -4,37 +4,41 @@ public class Runner {
     public static void main(String[]arg){
 
         Scanner scan = new Scanner(System.in);
+        String expression = "";
+
+        while (!expression.equals("-1")){
+            System.out.print("Enter an expression wih *, /, +, -, %, or ^: ");
+            expression = scan.nextLine();
+            if(expression.equals("-1")){
+
+            }else {
+                double num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
+                double num2 = Double.parseDouble(expression.substring(expression.indexOf(" ") + 3));
+                String symbol = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
 
 
-        System.out.print("Enter an expression wih *, /, +, -, %, or ^: ");
-        String expression = scan.nextLine();
+                Calculator calc = new Calculator(num1, num2);
 
-        double num1 = Double.parseDouble(expression.substring(0, expression.indexOf(" ")));
-        double num2 = Double.parseDouble(expression.substring(expression.indexOf(" ")+3));
-        String symbol = expression.substring(expression.indexOf(" ")+1, expression.indexOf(" ")+2);
-
-
-        Calculator calc = new Calculator(num1, num2);
-
-        if(symbol.equals("+")){
-            System.out.println("The answer is: "+calc.display(calc.add(num1, num2)));
+                if (symbol.equals("+")) {
+                    System.out.println("The answer is: " + calc.display(calc.add(num1, num2)));
+                }
+                if (symbol.equals("-")) {
+                    System.out.println("The answer is: " + calc.display(calc.subtract(num1, num2)));
+                }
+                if (symbol.equals("*")) {
+                    System.out.println("The answer is: " + calc.display(calc.multiply(num1, num2)));
+                }
+                if (symbol.equals("/")) {
+                    System.out.println("The answer is: " + calc.display(calc.divide(num1, num2)));
+                }
+                if (symbol.equals("%")) {
+                    System.out.println("The answer is: " + calc.display(calc.mod(num1, num2)));
+                }
+                if (symbol.equals("^")) {
+                    System.out.println("The answer is: " + calc.display(calc.powerOf(num1, num2)));
+                }
+            }
         }
-        if(symbol.equals("-")){
-            System.out.println("The answer is: "+calc.display(calc.subtract(num1, num2)));
-        }
-        if(symbol.equals("*")){
-            System.out.println("The answer is: "+calc.display(calc.multiply(num1, num2)));
-        }
-        if(symbol.equals("/")){
-            System.out.println("The answer is: "+calc.display(calc.divide(num1, num2)));
-        }
-        if(symbol.equals("%")){
-            System.out.println("The answer is: "+calc.display(calc.mod(num1, num2)));
-        }
-        if(symbol.equals("^")){
-            System.out.println("The answer is: "+calc.display(calc.powerOf(num1, num2)));
-        }
-
-
     }
+
 }
